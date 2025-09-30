b0dda4f4410bf90a58b748f1848d5a7acca52a7b
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-09-30 19:04:46 +0530

    feat: fetch patients functions added

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express")
-const { registerPatient } = require("../controllers/patientController")
+const { registerPatient, getAllPatients, getPatientById } = require("../controllers/patientController")
+const { protectedRoutes } = require("../middleware/protectedRoutes")
 
 const patientRouter = express.Router()
 
 patientRouter.post("/register", registerPatient)
+patientRouter.get("/getPatient", protectedRoutes, getAllPatients)
+patientRouter.get("/getPatient/:id", protectedRoutes, getPatientById)
 
 module.exports = { patientRouter }
