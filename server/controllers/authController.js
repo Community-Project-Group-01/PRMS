3ebc695113319d8c2672d07ceaa55f5b62657b89
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-10-01 21:25:56 +0530

    feat: authRoutes and login controller added

---
+++ server/controllers/authController.js
@@ -0,0 +1,27 @@
+const { User } = require("../models/User")
+const bcrypt = require("bcrypt")
+const { generateToken } = require("../utils/generateToken")
+
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: "All fields Required" })
+        }
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(404).json({ success: false, message: "No user found" })
+        }
+        const verifyPassword = await bcrypt.compare(password, user.password)
+        if (!verifyPassword) {
+            return res.status(401).json({ success: false, message: "Incorrect password" })
+        }
+        generateToken(user.id, res)
+        return res.status(200).json({ success: true, message: "Login successfull" })
+    } catch (error) {
+        console.error("Error in loginUser:", error.message);
+        return res.status(500).json({ success: false, message: "Server error" });
+    }
+}
+module.exports = { loginUser }
