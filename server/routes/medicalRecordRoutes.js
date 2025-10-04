996f4e8d84c6483541de7681b5d9ca295b9a8e03
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-10-04 12:03:11 +0530

    feat: medical records controller added

--- server/routes/medicalRecordRoutes.js
+++ server/routes/medicalRecordRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const { protectedRoutes } = require("../middleware/protectedRoutes")
 const { auditMiddleware } = require("../middleware/auditMiddleware")
-const { createMedicalRecord } = require("../controllers/medicalRecordController")
+const { createMedicalRecord, getRecordsByPatient, getRecordById } = require("../controllers/medicalRecordController")
 
 const medicalRecordRouter = express.Router()
 
-medicalRecordRouter.post("/add/:id", protectedRoutes, auditMiddleware("CREATED_MEDICAL_RECORD"), createMedicalRecord)
+medicalRecordRouter.post("/add/:id", protectedRoutes, createMedicalRecord, auditMiddleware("CREATED_MEDICAL_RECORD"))
+medicalRecordRouter.get("/patient/:id", protectedRoutes, getRecordsByPatient)
+medicalRecordRouter.get("/record/:id", protectedRoutes, getRecordById)
 
 module.exports = { medicalRecordRouter }
