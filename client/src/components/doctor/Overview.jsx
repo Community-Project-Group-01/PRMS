1eed0cba72bb961e6a8073e145df4a7badb50e5a
rashimarasangi <rashimarasangi200@gmail.com>
2025-11-08 13:28:07 +0530

    overview section is added

--- client/src/components/doctor/Overview.jsx
+++ client/src/components/doctor/Overview.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Overview = () => {
+  const [prescriptions, setPrescriptions] = useState([]);
+  useEffect(()=>{
+  const fetchPrescription = ()=>{
+
+  }
+  },[])
   return <div>Overview</div>;
 };
 
